7ba3ea91a3bfb06dc71e5e8287a05c30ac5b4959
yuyudifiesh <yizhixianyuuuu@gmail.com>
2026-04-12 17:45:14 +0800

    增加搜索功能

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -39,6 +39,32 @@ export default defineConfig({
       }
     ],
 
+    // 搜索
+    search: {
+      provider: 'local',
+      options: {
+        locales: {
+          root: {
+            translations: {
+              button: {
+                buttonText: '搜索',
+                buttonAriaLabel: '搜索'
+              },
+              modal: {
+                noResultsText: '无法找到相关结果',
+                resetButtonTitle: '清除查询条件',
+                footer: {
+                  selectText: '选择',
+                  navigateText: '切换'
+                }
+              }
+            }
+          }
+        }
+      }
+    },
+
+
     socialLinks: [
       { icon: 'github', link: 'https://github.com/yuyudifiesh/fastdev-html' }
     ],
